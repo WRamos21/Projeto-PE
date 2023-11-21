6e5b7c3f6d39f82cc0d753f6fa674c106d35d0b8
Romario Negreiros Baldarenas Silva <nromario482@gmail.com>
2023-11-21 20:36:17 -0300

    Criar definições de tipo para getBigNumber e printBigNumber, criar stuct BigNumber

--- bignumber.h
+++ bignumber.h
@@ -1,6 +1,16 @@
 #ifndef BIGNUMBER_H
 #define BIGNUMBER_H
 
-int add (int a, int b);
+// ????? tentar explicar depois
+#define MAX_SIZE __SIZE_MAX__ / 5
+
+typedef struct {
+    char *elements;
+    int size;
+} *BigNumber;
+
+BigNumber getBigNumber();
+
+void printBigNumber(BigNumber a);
 
 #endif
